d4ab78a0ee0fe0f45a01aedff41662490fc9a809
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-04-28 16:19:31 +0100

    add pin setup to reader.ino

--- reader/reader.ino
+++ reader/reader.ino
@@ -1,9 +1,47 @@
+#include <SPI.h>
+
 #define BAUDRATE 38400
+#define CLK 13
+#define DATA 12
+#define CMD 11
+#define ATT 10
+#define ACK 2
+
+volatile int ackState = HIGH;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(BAUDRATE);
+  while (!Serial) {
+    ; // wait for serial port to connect
+  }
+
+  setPins();
 }
+
+void setPins() {
+   // DATA from card (MISO)
+   pinMode(DATA, INPUT_PULLUP);
+   // CMD to card (MOSI)
+   pinMode(CMD, OUTPUT);
+   // ATT to card (SS)
+   pinMode(ATT, OUTPUT);
+   // CLK signal from PSX to card. 250KHz. (SCK)
+   pinMode(CLK, OUTPUT);
+   // ACK from card to PSX
+   pinMode(ACK, INPUT_PULLUP);
+
+   digitalWrite(CMD, LOW);
+   digitalWrite(ATT, HIGH);
+   digitalWrite(CLK, HIGH);
+
+   attachInterrupt(digitalPinToInterrupt(ACK), acknowledge, FALLING);
+}
+
+void acknowledge() {
+  ackState = !ackState;
+}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -11,7 +49,7 @@ void loop() {
   if(Serial.available() > 0)
   {
     // read the incoming byte
-    incomingByte = Serial.read();
+    byte incomingByte = Serial.read();
 
     // say what you got:
     Serial.print("I received: ");

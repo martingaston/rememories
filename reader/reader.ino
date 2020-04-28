4e76fe5edec70854711e9179722edd87447b89d6
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-04-28 22:48:52 +0100

    add SPI interface for reading frame from memory card

--- reader/reader.ino
+++ reader/reader.ino
@@ -46,6 +46,53 @@ void acknowledge() {
   ackState = !ackState;
 }
 
+byte command(byte toSend, int wait) {
+  ackState = HIGH;
+
+  byte recieved = SPI.transfer(toSend);
+
+  while(ackState == HIGH) {
+    wait--;
+    delayMicroseconds(1);
+    if(wait == 0) {
+      break;
+    }
+  }
+
+  return recieved;
+}
+
+void readFrame(unsigned int address) {
+  byte addressMSB = (address >> 8) & 0xFF;
+  byte addressLSB = address & 0xFF;
+  
+  digitalWrite(ATT, LOW);
+
+  SPI.beginTransaction(settings);
+  command(0x81, 500); // memory card access
+  command(0x52, 500); // send read 'R' command
+  command(0x00, 500); // receive memory card ID1
+  command(0x00, 500); // receive memory card ID2
+  command(addressMSB, 500); // send address MSB
+  command(addressLSB, 500); // send address LSB
+  command(0x00, 2800); // receive command acknowledge 1
+  command(0x00, 2800); // receive command acknowledge 2
+  command(0x00, 2800); // receive confirmed address MSB
+  command(0x00, 2800); // receive confirmed address LSB
+
+  // receive data sector (128 bytes)
+  for (int i = 0; i < 128; i++) {
+    Serial.write(command(0x00, 150));
+  }
+
+  Serial.write(command(0x00, 500)); // checksum (MSB xor LSB xor data)
+  Serial.write(command(0x00, 500)); // memory card end byte, should be 0x47
+  
+  SPI.endTransaction();
+
+  digitalWrite(ATT, HIGH);
+}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -53,13 +100,9 @@ void loop() {
   if(Serial.available() > 0)
   {
     // read the incoming byte
-    byte incomingByte = Serial.read();
-
-    // say what you got:
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
-    
+    byte incomingByte = Serial.read();    
     delay(10);
+    readFrame(0); // hardcode test first frame on card
   }
 
 }

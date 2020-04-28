a83edd5cb191d93914cb1cb828873b9f83aa8c37
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-04-28 23:29:04 +0100

    update writer.py to test connection can be made

--- writer.py
+++ writer.py
@@ -1,7 +1,10 @@
 import serial
+import time
 
-ser = Serial.serial('/dev/ttyUSB0')
-ser.write(b'88')
-connection_check = ser.read(4)
-
-print(connection_check.decode('utf-8')
+ser = serial.Serial(port='/dev/cu.usbmodem14201', baudrate=38400)
+time.sleep(3) # Arduino restarts when serial connection is made, very important else program will just hang forever :D
+ser.write(b'\x00')
+ser.read(128)
+ser.read(1)
+print(ser.read(1))
+ser.close()

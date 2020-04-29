af38d6e2a03280e6afd35849afc2438ee52f8937
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-04-29 22:21:33 +0100

    remove magic numbers and add argument parsing to remove hardcoded port and filename

--- writer.py
+++ writer.py
@@ -2,14 +2,15 @@ import serial
 import time
 import struct
 
-ser = serial.Serial(port='/dev/cu.usbmodem14201', baudrate=38400)
-time.sleep(3) # Arduino restarts when serial connection is made, very important else program will just hang forever :D
+FRAME_SIZE = 128
+FRAME_COUNT = 1024
+BAUDRATE = 38400
 
 def read_frame(frame_number):
     frame_as_bytes = struct.pack('>H', frame_number)
     ser.write(b'\x00' + frame_as_bytes)
 
-    data = ser.read(128)
+    data = ser.read(FRAME_SIZE)
     checksum = ser.read(1)
     status = ser.read(1)
 
@@ -18,10 +19,22 @@ def read_frame(frame_number):
 
     return data
 
-with open('memorycard.mcr', 'wb') as f:
-    for frame_number in range(1024):
-        print(f"{frame_number} / 1024 frames read to file")
-        data = read_frame(frame_number)
-        f.write(data)
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser("A command-line interface for reading a PlayStation 1 memory card to a file")
+    parser.add_argument("serialport", help=f"the serial port connected to the Arduino, e.g. /dev/cu.usbmodem14201 or /dev/ttyACM0")
+    parser.add_argument("-f", "--file", help="the name of the file to write to, default to memcard.mcr", default="memcard.mcr")
+    return parser.parse_args()
 
-ser.close()
+if __name__ == "__main__":
+    args = parse_args()
+    ser = serial.Serial(port=args.serialport, baudrate=BAUDRATE)
+    time.sleep(3) # Arduino restarts when serial connection is made, very important else program will just hang forever :D
+
+    with open(args.file, 'wb') as f:
+        for frame_number in range(FRAME_COUNT):
+            print(f"{frame_number} / {FRAME_COUNT} frames read to file")
+            data = read_frame(frame_number)
+            f.write(data)
+
+    ser.close()
